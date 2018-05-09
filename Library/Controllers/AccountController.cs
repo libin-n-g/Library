4881b2b5227176f8e52e8d95440896368be9be67
libin-n-g <libinngeorge@gmail.com>
2018-05-09 13:11:00 +0530

    Removes Unnessary Files and related Changes

--- Library/Controllers/AccountController.cs
+++ Library/Controllers/AccountController.cs
@@ -154,10 +154,6 @@ namespace Library.Controllers
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
-
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
